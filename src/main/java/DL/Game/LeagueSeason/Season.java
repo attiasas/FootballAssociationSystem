822e738534a7225b743f5a49dcb7965ba35a4135
AmirGabayy <49371295+AmirGabayy@users.noreply.github.com>
2020-04-03 14:59:41 +0300

    Uploading the Users Package and Games not including Events and Policies. This commit has no algorithms but mostly attributes and constructors. Added also empty constructors to every class.

--- src/main/java/DL/Game/LeagueSeason/Season.java
+++ src/main/java/DL/Game/LeagueSeason/Season.java
@@ -1,10 +1,30 @@
 package DL.Game.LeagueSeason;
 
+import javax.persistence.*;
+
 /**
- * Description:     X
+ * Description:     Represents a Season
  * ID:              X
  **/
+
+@Entity
+@NamedQueries( value = {
+        @NamedQuery(name = "Season", query = "SELECT s From Season s WHERE s.year = :year")
+})
 public class Season
 {
+
+    @Id
+    private int year;
+
+    public Season (int year)
+    {
+        this.year = year;
+    }
+
+    public Season()
+    {
+        this(0);
+    }
 
 }
--- src/main/java/DL/Users/User.java
+++ src/main/java/DL/Users/User.java
@@ -1,10 +1,86 @@
 package DL.Users;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
 
 /**
- * Description:     X
+ * Description:     Represents a user in the system
  * ID:              X
  **/
+@MappedSuperclass
+@NamedQueries( value = {
+        @NamedQuery(name = "UserByUsername", query = "SELECT u From User u WHERE u.username = :username"),
+        @NamedQuery(name = "UserByUsernameAndPassword", query = "SELECT u FROM User u WHERE u.username = :username AND u.hashedPassword = :hashedPassword")
+})
 public abstract class User
 {
-    private String userName;
+    @Id
+    private String username;
+    @Column
+    private List<String> searches;
+    @Column
+    private String email;
+    @Column
+    private String hashedPassword;
+    @OneToOne
+    private UserPermission userPermission;
+    @OneToMany
+    private HashMap<Notification, Boolean> notificationsOwner; //maps from notification to a boolean of read or not read
+    @OneToMany
+    private List<UserComplaint> userComplaintsOwner;
+
+
+
+
+    /**
+     *constructor with PermissionList
+     * @param userName
+     * @param email
+     * @param hashedPassword
+     * @param permissionList
+     */
+    public User (String userName, String email, String hashedPassword, List<UserPermission.Permission> permissionList)
+    {
+        this.username = userName;
+        this.searches = new ArrayList<String>();
+        this.email = email;
+        this.hashedPassword = hashedPassword;
+        this.userPermission = new UserPermission(permissionList);
+        this.notificationsOwner = new HashMap<Notification,Boolean>();
+        this.userComplaintsOwner = new ArrayList<UserComplaint>();
+    }
+
+    /**
+     * constructor without PermissionList
+     */
+    public User (String userName, String email, String hashedPassword)
+    {
+        this(userName, email, hashedPassword, new ArrayList<UserPermission.Permission>());
+    }
+
+    public User ()
+    {
+        this("", "", "");
+    }
+
+    public void addComplaint(UserComplaint userComplaint)
+    {
+        this.userComplaintsOwner.add(userComplaint);
+    }
+
+    public boolean hasPermission(UserPermission.Permission permission)
+    {
+        return userPermission.hasPermission(permission);
+    }
+
+
+    @Override
+    public String toString ()
+    {
+        return this.username;
+    }
+
+
 }
